3cbd0f95d83ff9ac25e5cc2fff56dde8f5e2769b
roml <romell.pineda@gmail.com>
2025-02-06 20:15:03 -0800

    add job board

---
+++ WpfApp/Models/JobManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    public class JobManager
+    {
+        public static ObservableCollection<Job> _JobDatabase = new ObservableCollection<Job>() { new Job {Title = "DevOps Engineer", Description = "Lorem..."}, new Job {Title = "Software Engineer", Description = "Lorem..."}, new Job {Title = "Project Manager", Description = "Lorem.." } };
+
+        public static ObservableCollection<Job> GetJobs()
+        {
+            return _JobDatabase;
+        }
+
+        public static void AddJob(Job job)
+        {
+            _JobDatabase.Add(job);
+        }
+    }
+}
---
+++ WpfApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp.Commands;
+using WpfApp.Models;
+using WpfApp.Views;
+
+namespace WpfApp.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Job> Jobs { get; set; }
+
+        public ICommand ShowWindowCommand { get; set; }
+
+        public MainViewModel()
+        {
+            Jobs = JobManager.GetJobs();
+            ShowWindowCommand = new RelayCommand(ShowWindow, CanShowWindow);
+        }
+
+        private bool CanShowWindow(object obj)
+        {
+            return true;
+        }
+
+        private void ShowWindow(object obj)
+        {
+            ApplyToJob applyToJob = new ApplyToJob();
+            applyToJob.Show();
+        }
+    }
+}
